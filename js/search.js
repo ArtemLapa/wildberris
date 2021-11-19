10534f91dfe19364249841f223b4665fba3da2c8
Artem Lapa <artyom.lapa@gmail.com>
2021-11-19 14:55:14 +0200

    feat: change eventListener for searchBtn

--- js/search.js
+++ js/search.js
@@ -2,15 +2,8 @@ const search = function () {
   const input = document.querySelector(".search-block > input"),
     searchBtn = document.querySelector(".search-block > button");
   
-
-  let inputValue = ""
-
-  input.addEventListener("input", (e) => {
-    inputValue = e.target.value;
-  });
-  
   searchBtn.addEventListener("click", () => {
-    console.log(inputValue)
+    console.log(input.value)
   });
 };
 
